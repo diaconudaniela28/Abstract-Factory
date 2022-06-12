91c5c589d083bfce4e1dbbb31701dbfe76990210
diaconudaniela28 <89979866+diaconudaniela28@users.noreply.github.com>
2022-06-13 00:42:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Abstract__Factory.Factories;
+using Abstract__Factory.ShirtProducts;
+using Abstract__Factory.TrousersProducts;
+using System;
+
+namespace Abstract__Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClothesFactory factory = new CasualClothesFactory();
+
+            Shirt shirt = factory.CreateShirt();
+            Trousers trousers = factory.CreateTrousers();
+
+            shirt.LookOfDay();
+            trousers.DescribeYourClothes();
+
+        }
+    }
+}
